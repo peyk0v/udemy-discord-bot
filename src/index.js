479690952cd2d92fee69be77096c8004f0192b06
peykov <peykovn@gmail.com>
2022-03-27 21:19:58 -0300

    first ds approach

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 require('dotenv').config()
+const { Client, Intents } = require('discord.js')
 const openDatabaseConnection = require('./db')
 const coursesChecker = require('./courses')
-const Course = require('./schemas/course')
+const { REGEXS } = require('./utils')
+const sendHelpText = require('./handlers/help')
+const setChannel = require('./handlers/setChannel')
 
-openDatabaseConnection()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.login(process.env.BOT_TOKEN)
+
+client.on("ready", () => {
+  openDatabaseConnection()
+  console.log("bot is ready")
+})
+
+client.on("messageCreate", (msg) => {
+  if(msg.member.user.bot) return;
+
+  if(msg.content.match(REGEXS.SET_CHANNEL)) {
+    setChannel(msg)
+  } else if(msg.content.match(REGEXS.HELP)) {
+    sendHelpText(msg)
+  }
+
+})
+
+
+
+//TODO: lo de abajo habria que borrarlo, pura mierda 
 
 async function saveCourses() {
   const coursesToPublish = await coursesChecker.checkForNewCourses()
-  console.log({coursesToPublish})
 
+  //IMPORTANTE CHEQUEAR ESTO
+  if(coursesToPublish.length == 0) {
+    console.log('there is no new courses')
+    return
+  }
+
+  console.log({coursesToPublish})
   for(let course of coursesToPublish) {
     const newCourse = new Course(course)
     await newCourse.save()
@@ -19,5 +50,14 @@ async function resetDB() {
   await Course.deleteMany({})
 }
 
-saveCourses()
-//resetDB()
+async function createTestServer() {
+  const server = {
+    server_id: '915045284562731108',
+    channel_id: '957721127910793296'
+  }
+
+  const newServer = new Subscriber(server)
+  await newServer.save()
+}
+
+
