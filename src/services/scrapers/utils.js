a9dfa5817d8d744bf4a72a1376948b6cc6e8d946
peykov <peykovn@gmail.com>
2022-04-02 01:03:26 -0300

    fix puppeteer launch on heroku

--- src/services/scrapers/utils.js
+++ src/services/scrapers/utils.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 async function createPageIn(link) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
   await page.setViewport({width:960,height:768});
